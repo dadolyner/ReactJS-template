c60cfa2ec2f6d1e651ed7892b23081db425f3537
dadolyner <skulj.david@gmail.com>
2022-06-23 12:40:03 +0200

    initial commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import '../styles/GlobalStyles.css';
+
+const App: FC = () => {
+    return (
+        <>
+            <h1>Application</h1>
+        </>
+    );
+};
+
+export default App;
